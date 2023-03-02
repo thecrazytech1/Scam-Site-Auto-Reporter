4ab3d03e35129058b6c38d0afa02c0b646b5447f
ChrissQuartz <chrissquartz@gmail.com>
2023-03-03 10:31:56 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function submitInfo(event) {
+  event.preventDefault();
+
+  var email = document.getElementById("email").value;
+  var url = document.getElementById("url").value;
+  var category = document.getElementById("category").value;
+  var reason = document.getElementById("reason").value;
+
+  if (!email || !url || !reason) return alert("Please fill out all fields");
+  if (email.trim() === "" || url.trim() === "" || reason.trim() === "")
+    return alert("Please fill out all fields");
+
+  //   check if email is valid
+  if (!validateEmail(email)) return alert("Please enter a valid email");
+  if (!validateUrl(url)) return alert("Please enter a valid url");
+
+  var data = {
+    email: email,
+    url: url,
+    category: category,
+    reason: reason,
+  };
+
+  console.log(data);
+}
+
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+function validateUrl(url) {
+  var re = /^(http|https):\/\/[^ "]+$/;
+  return re.test(url);
+}
